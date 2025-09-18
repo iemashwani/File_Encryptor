6f343b526f79d26bebfe9ba23cbb1f56643fa84f
AshwaniSingh12 <as3141538@gmail.com>
2025-09-18 22:32:34 +0530

    Add Serialization and Deserialization

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include "../fileHandling/IO.hpp"
+#include <sstream>
 
 using namespace std;
 
@@ -19,11 +20,42 @@ struct Task
     fstream f_stream;
     Action action;
 
-    Task(fstream &&stream, Action act, string filePath){
+    Task(fstream &&stream, Action act, string filePath)
+    {
         this->filePath = filePath;
         this->action = act;
         f_stream = move(stream);
     }
+
+    // serialization
+    string toString()
+    {
+        ostringstream oss;
+        oss << filePath << "," << (action == Action::ENCRYPT ? "ENCRYPT" : "DECRYPT");
+    }
+
+    // deserialization
+    static Task fromString(const string &taskData){
+        istringstream iss(taskData);
+        string filePath;
+        string actionStr;
+
+        if(getline(iss, filePath, ',') && getline(iss, actionStr)){
+            Action action = (actionStr == "ENCRYPT" ? Action::ENCRYPT : Action::DECRYPT);
+            IO io(filePath);
+            fstream f_stream = move(io.getFileStream());
+            if(f_stream.is_open()){
+                return Task(move(f_stream), action, filePath);
+            } else {
+                throw runtime_error("Failed to open file: "+ filePath);
+            }
+        } else {
+            throw runtime_error("Invalid task data format");
+        }
+    }
+    
+
+
 };
 
 #endif
