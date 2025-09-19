a570d69db2c3fdb673325b79a5826523a06f631e
AshwaniSingh12 <as3141538@gmail.com>
2025-09-19 22:10:33 +0530

    create Process Management

---
+++ src/app/processes/ProcessManagement.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "ProcessManagement.hpp"
+#include <unistd.h>
+#include <cstring>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    #include <sys/wait.h>
+#endif
+// #include "../encryptDecrypt/Cryption.hpp"
+
+using namespace std;
+
+ProcessManagement::ProcessManagement() {}
+
+bool ProcessManagement::submitToQueue(unique_ptr<Task> task) {
+    taskQueue.push(move(task));
+    return true;
+}
+
+
+void ProcessManagement::executeTasks(){
+    while(!taskQueue.empty()){
+        unique_ptr<Task> taskToExecute = move(taskQueue.front());
+        taskQueue.pop();
+        cout << "Executing task: " << taskToExecute->toString() << endl;
+        executeCryption(taskToExecute->toString());
+    }
+}
